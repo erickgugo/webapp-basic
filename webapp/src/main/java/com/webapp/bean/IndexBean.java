3f2f3b84f57397517c136907c55df58710e6232a
Andre Crespo <andrecr6600@gmail.com>
2022-08-29 13:50:55 -0400

    boton de anadido aumentado

--- webapp/src/main/java/com/webapp/bean/IndexBean.java
+++ webapp/src/main/java/com/webapp/bean/IndexBean.java
@@ -2,17 +2,14 @@ package com.webapp.bean;
 
 
 import com.webapp.entity.User;
-import com.webapp.entity2.MainUsers;
-import com.webapp.entity2.New_User_list;
-import com.webapp.entity2.User_list;
 //import com.webapp.entity2.MainUsers;
 
-import com.webapp.entity2.User_list;
 import lombok.Data;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +21,7 @@ import java.util.List;
 @ViewScoped
 public class IndexBean {
 
-private List<String> listNombre = new ArrayList<>();
+    private List<String> listNombre = new ArrayList<>();
 
     private User user;
     private List<User> listUser = new ArrayList<>();
@@ -38,8 +35,10 @@ private List<String> listNombre = new ArrayList<>();
 
     public void actionAdd() {
         System.out.println(user);
-        listUser.add(user);
-        user = new User();
+        //listUser.add(user);
+        insertUser(user);
+        init();
+        listUser = getList();
     }
 
     public void actionPrint() {
@@ -47,4 +46,105 @@ private List<String> listNombre = new ArrayList<>();
             System.out.println("===> " + user1);
         }
     }
+
+    public void insertUser(User usuario) {
+        System.out.println("Insert: " + usuario);
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://127.0.0.1:3306/facebook";
+            String user = "root";
+            String password = "";
+
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println(conn);
+
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO " +
+                    "usuarios(nombre, apellido, telfono, email, usuario, clave) " +
+                    "VALUES (" +
+                    usuario.getNombre() + "," +
+                    usuario.getApellido() + "," +
+                    usuario.getTelefono() + "," +
+                    usuario.getEmail() + "," +
+                    usuario.getUsuario() + "," +
+                    usuario.getClave() +
+                    ")";
+            System.out.println(sql);
+            stmt.executeUpdate(sql);
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+
+    public List<User> getList()
+    {
+
+        //System.out.println("Insert: "+usuario);
+    Connection conn = null;
+        try
+
+    {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String url = "jdbc:mysql://127.0.0.1:3306/facebook";
+        String user = "root";
+        String password = "";
+
+        conn = DriverManager.getConnection(url, user, password);
+        System.out.println(conn);
+
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select* from Usuarios");
+        User u;
+        while (rs.next()) {
+            u = new User();
+
+            System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getInt(4) + " " + rs.getString(5) + " " + rs.getString(6) + "     " + rs.getString(7));
+            u.setId(rs.getInt(1));
+            u.setNombre(rs.getString(2));
+            u.setApellido(rs.getString(3));
+            u.setTelefono(rs.getInt(4));
+            u.setEmail(rs.getString(5));
+            u.setUsuario(rs.getString(6));
+            u.setClave(rs.getString(7));
+            listUser.add(u);
+            //List<User> listUser= new ArrayList<>();
+
+        }
+    } catch(
+    Exception ex)
+
+    {
+        ex.printStackTrace();
+    }finally
+
+    {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    //CODE
+//return u;
+       return listUser;
+}
+
 }
--- webapp/src/main/java/com/webapp/entity/User.java
+++ webapp/src/main/java/com/webapp/entity/User.java
@@ -11,4 +11,6 @@ public class User {
     private String email;
     private String usuario;
     private String clave;
+
+
 }
